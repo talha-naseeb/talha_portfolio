8a6121dcb92499fad193c67033281a5552fc47b5
talha-naseeb <talhanaseeb27@gmail.com>
2025-04-21 01:35:13 +0500

    add the updated resume url

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,6 +1,6 @@
 export const Bio = {
   name: "Talha Naseeb",
-  roles: ["Frontend Developer", "Ui/Ux Expert", "Programmer"],
+  roles: ["Mern Stack Developer", "Ui/Ux Expert", "Programmer"],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/talha-naseeb",
